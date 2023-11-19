d48488bc8108ad218c8f74976f30eee5fe32907a
Frank <fpatino@fi.uba.ar>
2023-11-19 20:47:22 -0300

    fix configuracion del proyecto

---
+++ src/main/java/psa/api_proyectos/models/ProyectoModel.java
@@ -0,0 +1,48 @@
+package psa.api_proyectos.models;
+
+import javax.persistence.*;
+import java.util.Date;
+@Table(name = "Proyectos")
+@Entity
+public class ProyectoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private long id;
+    private String nombre;
+    private String descripcion;
+    private Date fechaInicio;
+    private Date fechaFin;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+}
