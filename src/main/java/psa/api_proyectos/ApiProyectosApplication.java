202bcbf320cb7c40d16c3284ff65dedb1a4194d7
Frank <fpatino@fi.uba.ar>
2023-11-19 17:41:47 -0300

    primer commit
    configuración base del proyecto: estructura y dependencias para JPA y MySQL

---
+++ src/main/java/psa/api_proyectos/ApiProyectosApplication.java
@@ -0,0 +1,13 @@
+package psa.api_proyectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProyectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProyectosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/psa/api_proyectos/ApiProyectosApplicationTests.java
@@ -0,0 +1,13 @@
+package psa.api_proyectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProyectosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
