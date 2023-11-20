4b3aced013d15f5b2a8fb7d5f9ffef2f588bd0af
GLOBALAD\ivan.azuaje <ivan.azuaje@globalsis.com.ar>
2023-11-20 20:18:48 -0300

    Agregado de dependencias para el correcto funcionamiento de entorno de pruebas usando cucumber y Junit. Creado de prueba placeholder para comprobar funcionamiento.

---
+++ src/test/java/psa/api_proyectos/cucumber/CucumberBootstrap.java
@@ -0,0 +1,17 @@
+package psa.api_proyectos.cucumber;
+
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+@Slf4j
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CucumberBootstrap {
+
+    @Autowired
+    protected TestRestTemplate testRestTemplate;
+}
---
+++ src/test/java/psa/api_proyectos/cucumber/ProyectosApiIntegrationTest.java
@@ -0,0 +1,7 @@
+package psa.api_proyectos.cucumber;
+
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+public class ProyectosApiIntegrationTest {
+}
