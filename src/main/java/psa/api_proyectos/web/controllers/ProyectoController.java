477d35b6b47a78b7139b21ae1ba4166aff87083a
Camila Bartocci <65984154+bitapricot@users.noreply.github.com>
2023-11-29 01:07:38 -0300

    borro imports

--- src/main/java/psa/api_proyectos/web/controllers/ProyectoController.java
+++ src/main/java/psa/api_proyectos/web/controllers/ProyectoController.java
@@ -1,6 +1,5 @@
 package psa.api_proyectos.web.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import psa.api_proyectos.application.dtos.ProyectoDto;
 import psa.api_proyectos.application.dtos.TareaDto;
 import psa.api_proyectos.application.exceptions.*;
-import psa.api_proyectos.application.services.RecursosService;
 import psa.api_proyectos.application.services.TareaService;
 import psa.api_proyectos.domain.models.Proyecto;
 import psa.api_proyectos.domain.models.Tarea;
